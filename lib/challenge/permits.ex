8996f20b198912dbe6e569f22fa91d7db1ff514f
Josh Hogan <josh.hogan@me.com>
2024-09-04 13:11:17 -0400

    Define function to list permits, optionally filtered by status

--- lib/challenge/permits.ex
+++ lib/challenge/permits.ex
@@ -3,6 +3,8 @@ defmodule Challenge.Permits do
   Context module used to perform actions with permits.
   """
 
+  import Ecto.Query, only: [from: 2]
+
   alias Challenge.Repo
   alias Challenge.Permits.Permit
 
@@ -15,4 +17,32 @@ defmodule Challenge.Permits do
     |> Permit.changeset(attrs)
     |> Repo.insert()
   end
+
+  @doc """
+  Lists permits based on the parameters provided.
+  """
+  @spec list_permits(map()) :: list(Permit.t())
+  def list_permits(params \\ %{}) do
+    query =
+      from p in Permit,
+        as: :permit,
+        order_by: [asc: p.permit_holder]
+
+    query
+    |> build_query(params)
+    |> Repo.all()
+  end
+
+  defp build_query(query, params) do
+    Enum.reduce(params, query, &param_query/2)
+  end
+
+  defp param_query({"status", v}, query) do
+    v = String.downcase(v)
+
+    from [permit: permit] in query,
+      where: permit.status == ^v
+  end
+
+  defp param_query(_kv, query), do: query
 end
--- test/challenge/permits_test.exs
+++ test/challenge/permits_test.exs
@@ -23,4 +23,40 @@ defmodule Challenge.PermitsTest do
       assert {:error, %Ecto.Changeset{}} = Permits.create_permit(%{})
     end
   end
+
+  describe "list_permits/1" do
+    test "filter by status with case insensitivity" do
+      %{id: inserted_id} = %{"status" => "approved"} |> permit() |> insert!()
+
+      params = %{
+        "status" => "APPROVED"
+      }
+
+      assert [%Permit{id: ^inserted_id}] = Permits.list_permits(params)
+
+      params = %{
+        "status" => "expired"
+      }
+
+      assert [] = Permits.list_permits(params)
+    end
+  end
+
+  defp permit(attrs) do
+    %{
+      "location_id" => "1234",
+      "location_description" => "1ST ST => LYMAN AVE to TAPESTRY RDG",
+      "permit_number" => "23MFF-00030",
+      "permit_holder" => "Bill's Snacks",
+      "food_items" => "American Food: Hog dogs: pretzels: beverages",
+      "hours_of_operation" => "Mo-Fr:12PM-8PM",
+      "status" => "approved"
+    }
+    |> Map.merge(attrs)
+  end
+
+  defp insert!(attrs) do
+    {:ok, permit} = Permits.create_permit(attrs)
+    permit
+  end
 end
